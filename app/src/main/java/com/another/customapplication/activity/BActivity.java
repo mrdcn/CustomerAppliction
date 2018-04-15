6e650e5cd791cbad30f80e2051d127ebc2716004
miaorun <miaorun@wanda.cn>
2018-04-15 12:07:29 +0800

    RxJava & RxAndroid

--- app/src/main/java/com/another/customapplication/activity/BActivity.java
+++ app/src/main/java/com/another/customapplication/activity/BActivity.java
@@ -1,8 +1,20 @@
 package com.another.customapplication.activity;
 
 import android.os.Bundle;
+import android.os.Trace;
+import android.util.Log;
 
 import com.another.customapplication.R;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class BActivity extends MainActivity {
 
@@ -12,7 +24,56 @@ public class BActivity extends MainActivity {
         discriptor = "-----B";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+        test();
+
     }
+
+    public void test(){
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("a");
+                Thread.sleep(200);
+                emitter.onNext("b");
+                Thread.sleep(200);
+                emitter.onNext("c");
+                Thread.sleep(200);
+                emitter.onNext("d");
+                Log.e("rxjava_scheduler_" ,Thread.currentThread().getName());
+            }
+        });
+
+        observable.subscribeOn(Schedulers.io()).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e("rxjava_scheduler_" + System.currentTimeMillis(),Thread.currentThread().getName() + "\t" + s);
+            }
+        });
+
+    }
+
 
 
 
